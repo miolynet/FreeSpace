cd91736f111c1ff18ad7168d19e309030566e947
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2010-11-12 18:30:11 +0700

    initialize TeacherVideo project sample add silverlight bussiness application project add sql 2008 database project
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ Sample.SilverlightAnimationCPUUsage/Sample.SilverlightAnimationCPUUsage/BallUI.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Sample_SilverlightAnimationCPUUsage
+{
+	public partial class BallUI : UserControl
+	{
+		public BallUI()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
